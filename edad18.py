b871e39a0f2a2de7640db7816622fb41ac044133
q92helea <q92helea@hotmail.com>
2024-06-25 21:13:34 +0200

    inicio

---
+++ edad18.py
@@ -0,0 +1,8 @@
+# Pedir al usuario que ingrese su edad
+edad = int(input("Ingrese su edad: "))
+
+# Verificar si es menor de 18 años
+if edad < 18:
+    print("Eres menor de edad.")
+else:
+    print("Eres mayor de edad.")
