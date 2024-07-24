9ee1c18e4a97272c4a47a0f3154cf45fbc6a3b59
AsirH <45197222+asirheras@users.noreply.github.com>
2024-07-24 10:10:56 +0200

    ejercicios 01-02

---
+++ MicrosoftLEARN/Python.para.principiantes/20240724_01.fechahoy.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+# Obtener la fecha actual
+hoy = date.today()
+
+# Imprimir la fecha actual
+print("La fecha de hoy es: ", hoy)
---
+++ MicrosoftLEARN/Python.para.principiantes/20240724_02.parsecs.py
@@ -0,0 +1,8 @@
+# Define la variable parsecs
+parsecs = 11
+
+# Realice el cálculo para convertir Parsec(p) a Años Luz (ly)
+lightyears = parsecs * 3.26
+
+# Imprime el resultado
+print(f"{parsecs} parsecs is {lightyears} lightyears")
