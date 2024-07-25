b985f8e186f8e2b1da73772a3cac44c77a8526c1
AsirH <45197222+asirheras@users.noreply.github.com>
2024-07-25 21:17:53 +0200

    añadido ejercicio 'secreto'

---
+++ pdf.intro/secreto.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+clave = "secreto"
+entrada = input('Introduce la clave ')
+if entrada == clave:
+        print ("acertaste, la clave es "), entrada, ""
+else:
+        print ("acceso no autorizado")
--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-first_number = int(input('Tipea el primer número: ')) ;\
-second_number = int(input('Tipea el segundo número: ')) ;\
-print("La suma es: ", first_number + second_number)
